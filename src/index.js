9baa72d95a5ab4be50897e317493f9236593393d
Luizgustavo358 <luiz.braganca@sga.pucminas.br>
2020-11-02 23:24:05 -0300

    cors

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
@@ -8,6 +9,7 @@ mongoose.connect('mongodb+srv://Luizgustavo358:3649aqwh@cluster0.cuyt1.mongodb.n
     useUnifiedTopology: true
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
